144ac104d3ec82e80a55f29f04f80e7784e62ae5
Kamil <kamil_p7@o2.pl>
2019-01-28 22:07:05 +0000

    test cont.

--- src/tests/components/LineChart.test.js
+++ src/tests/components/LineChart.test.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { render } from "enzyme";
+import { shallow } from "enzyme";
 import LineChart from "../../LineChart";
+import { data } from "../fixtures/testData";
 
-test("should render component wtih no props provided", () => {
-  const wrapper = render(<LineChart chartData={[]} chartTitle={""} />);
+test("should render LineChart component with data and title props", () => {
+  const wrapper = shallow(
+    <LineChart chartData={data} chartTitle={"Average temperature for London"} />
+  );
   expect(wrapper).toMatchSnapshot();
 });
